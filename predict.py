f0aa926056be65cdd9d3782acbfc218dec13f3c9
LinhXinhGaiNhat <llslla19712001@gmail.com>
2024-04-17 12:20:13 +0700

    hi

--- predict.py
+++ predict.py
@@ -14,11 +14,11 @@ import os
 import streamlit as st
 @st.cache_resource
 def load_model():
-    if not os.path.isfile('model.h5'):
-        urllib.request.urlretrieve('https://github.com/leanhtu-AI/Sentiment-Analysis/raw/main/model/best.h5', 'model/model.h5')
+    if not os.path.isfile('best4_16_4_4.h5'):
+        urllib.request.urlretrieve('https://github.com/NguyenHuyHoangCome/steamlit/raw/main/model/best4_16_4_4.h5', 'model/best4_16_4_4.h5')
         pretrained_bert = TFAutoModel.from_pretrained(PRETRAINED_MODEL, output_hidden_states=True)
         reloaded_model = create_model(pretrained_bert)
-        reloaded_model.load_weights('model/model.h5')
+        reloaded_model.load_weights('model/best4_16_4_4.h5')
         return reloaded_model
 
 reloaded_model = load_model()
